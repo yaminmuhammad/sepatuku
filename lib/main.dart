f6dae69649256f6a6ab9348c14bc04c7f7ef0341
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-01-16 06:59:36 +0700

    feat: splashpage & routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:sepatuku/pages/sign_in_page.dart';
+import 'package:sepatuku/pages/splash_pages.dart';
 import 'theme.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+      },
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text('Hello',
-          style:
-              secondaryTextStyle.copyWith(fontWeight: semiBold, fontSize: 40)),
-    ));
   }
 }
---
+++ lib/pages/splash_pages.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:sepatuku/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  // const SplashPage({super.key});
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/sign-in'),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: backgroundColor1,
+        body: Center(
+          child: Container(
+            width: 130,
+            height: 150,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/image_splash.png'))),
+          ),
+        ));
+  }
+}
