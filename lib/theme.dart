4b4d94c8cbe34c482a6fe087583baa0067379e71
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-01-24 12:56:39 +0700

    feat: Hompage Categories

--- lib/theme.dart
+++ lib/theme.dart
@@ -14,6 +14,7 @@ Color backgroundColor4 = Color(0xff252836);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
+Color transparentColor = Colors.transparent;
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
