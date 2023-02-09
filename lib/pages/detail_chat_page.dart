cd8f35e1b2ae72a8e86d3fc71106e1426bf4b1e6
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-02-09 19:28:00 +0700

    detail chat page bag input 1

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -46,9 +46,47 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 45,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: backgroundColor4,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type Message ...',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Image.asset(
+              'assets/button_send.png',
+              width: 45,
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
