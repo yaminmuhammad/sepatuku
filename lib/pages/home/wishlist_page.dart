b487860816b296c8da5964ffd0feced150c901cf
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-02-10 16:46:25 +0700

    wishlist page content

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -6,13 +6,84 @@ class WishListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'WishList Page',
-          style: primaryTextStyle,
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Favorite Shoes',
         ),
-      ),
+        automaticallyImplyLeading: false,
+        elevation: 0,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 74,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                'You don\'t have favorite shoes?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Let\'s find your favorite shoes',
+                style: secondaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: regular,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 10,
+                  ),
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        emptyWishlist(),
+      ],
     );
   }
 }
