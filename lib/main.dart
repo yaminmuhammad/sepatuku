607220d1d4cba4d908d410fddcdf18cf5f64f009
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-02-13 09:30:29 +0700

    checkout header & items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sepatuku/pages/cart_page.dart';
+import 'package:sepatuku/pages/checkout_page.dart';
 import 'package:sepatuku/pages/detail_chat_page.dart';
 import 'package:sepatuku/pages/edit_profile_page.dart';
 import 'package:sepatuku/pages/home/main_page.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         '/edit-profile': (context) => EditProfilePage(),
         '/product': (context) => ProductPage(),
         '/cart': (context) => CartPage(),
+        '/checkout': (context) => CheckoutPage(),
       },
     );
   }
---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:sepatuku/theme.dart';
+import 'package:sepatuku/widgets/checkout_card.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Checkout Details',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          // NOTE: LIST ITEMS
+
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                CheckoutCard(),
+                CheckoutCard(),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
